02f7681f7f520d0535d05bf92cb742f825c37d17
JosueUN <a24310225@live.ceti.mx>
2025-03-08 00:47:15 +0000

    codigo base chumi

---
+++ Foco.hpp
@@ -0,0 +1,14 @@
+#include <Piedra.hpp>
+#include <Humo.hpp>
+
+class Foco
+{
+private:
+    Piedra piedra;
+public:
+    Foco() {}
+    ~Foco() {}
+    Humo Fumar (){
+        return piedra.Quemar();
+    }
+};
