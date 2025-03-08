c15b8af56557f8765c755bafc367387f3e0a264f
JosueUN <a24310225@live.ceti.mx>
2025-03-08 01:43:12 +0000

    Codigo base de mascota

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Mascota.hpp"
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"Ejemplo Mascota"<<std::endl;
+    Mascota firulais;
+    
+    for (size_t i =0 ; i<3 ; i++){
+        firulais.Golpear();
+    }
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        firulais.Acariciar();
+    }
+    
+
+    std::cout<<"Vida actual: "<< firulais.LeerVida()<<std::endl;
+
+    return 0;
+}
+
